43fcd1bde64dfa2ac490eab76e755e240832694f
LechDutkiewicz <lechdutkiewicz@gmail.com>
2017-07-04 20:08:44 +0200

    first-commit

---
+++ config.rb
@@ -0,0 +1,12 @@
+# Require any additional compass plugins here.
+require 'bootstrap-sass'
+
+preferred_syntax = :scss
+http_path = '/'
+css_dir = '/'
+sass_dir = 'assets/scss'
+images_dir = 'assets/images'
+javascripts_dir = 'assets/javascripts'
+relative_assets = true
+line_comments = true
+# output_style = :compressed
---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+function my_theme_enqueue_styles() {
+
+    $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+?>
